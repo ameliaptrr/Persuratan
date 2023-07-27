5049c46f6beac6cc68783cfac45f3e82dfe7b8af
ameliaptrr <140705141+ameliaptrr@users.noreply.github.com>
2023-07-27 17:11:31 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="assets/style.css" />
+<title>Layanan Pengaduan Masyarakat</title>
+</head>
+  <body>
+
+<div class="container">
+<div class="sidebar">
+    <div class="header">
+      <img src="assets/icon/user.webp" alt="">
+        </div>
+    <div class="main">
+        <div class="list-item">
+            <a href="userlist.php">User</a>
+        </div>
+        <div class="list-item">
+            <a href="adminlist.php">Admin</a>
+        </div>
+    <div class="list-item">
+        <a href="produklist.php">Produk</a>
+        </div>
+    <div class="list-item">
+        <a href="logout.php">Logout</a>
+        </div>
+    <div class="list-item">
+         <a href="#">Pesanan</a>
+        </div>
+         </div>
+         </div>
+    <div class="main-content">
+        <div class="nav">
+            <h1>WEB PKK</h1>
+        <Span class="judul">Admin</Span>
+        </div>
+    <div class="right-menu">
+        <a href="#"><img src="assets/icon/login.png" alt=""></a>
+        </div>
+        <div class="navig"></div>
+    <div class="content">
